6a28a4e229294b2da5bcade3d4ed9dbaf06fa9c1
CasMonDavid <dcastro_21@alu.uabcs.mx>
2024-06-13 08:38:17 -0700

    Add funcion in iniciarsesion y registrarsesion

--- frontend/src/components/paginas/inciarSesion/iniciarSesion.jsx
+++ frontend/src/components/paginas/inciarSesion/iniciarSesion.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom'
 import './iniciarSesion.css'
 import fondo from './caclog.jpg'
 import logo from './logo_uabcs.png'
+import Axios from 'axios';
 
 const IniciarSesion = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginTeacher = () => {
+        Axios.post("http://localhost:4000/iniciarsesion/investigador", { email, password })
+        .then(response => {
+          if (response.data.success) {
+            const userData = response.data.data;
+            localStorage.setItem('user', JSON.stringify(userData));
+            localStorage.setItem('isLoggedIn', true);
+            alert('Sesión iniciada con éxito');
+          } else {
+            alert('Correo o contraseña incorrectos');
+          }
+        })
+        .catch(error => {
+          console.error('Error al iniciar sesión:', error);
+        });
+    };
+
     return (
         <div className="iniciar-sesion">
             <div className="primera-parteIS">
@@ -16,9 +38,21 @@ const IniciarSesion = () => {
                     <h1>HERBARIO</h1>
                 </div>
                 <h1 className="subIS">Inicia sesión</h1>
-                <input type="email" placeholder="Correo" className="inIS"/>
-                <input type="password" placeholder="Contraseña" className="inIS"/>
-                <button className="botonIS">Iniciar sesión</button>
+                <input
+                    type="email"
+                    placeholder="Correo"
+                    className="inIS"
+                    onChange={(event)=>{
+                        setEmail(event.target.value);
+                    }}/>
+                <input
+                    type="password"
+                    placeholder="Contraseña"
+                    className="inIS"
+                    onChange={(event)=>{
+                        setPassword(event.target.value);
+                    }}/>
+                <button className="botonIS" onClick={loginTeacher}>Iniciar sesión</button>
                 <a className="linkIS" href="#">He olvidado mi contraseña</a>
                 <h1 className="textIS">¿Todavia no tienes cuenta? <span><Link to='/registrar' className="linkISD"> Registrate</Link></span></h1>
             </div>
--- frontend/src/components/paginas/registrarSesion/registrarSesion.jsx
+++ frontend/src/components/paginas/registrarSesion/registrarSesion.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import './registrarSesion.css'
 import fondoo from './caclog.jpg'
 import logoo from './logo_uabcs.png'
+import Axios from 'axios';
+
+//local: http://localhost:4000/registrar/user
+//railway: https://backherbario-production-7369.up.railway.app/registrar/user
 
 const RegistrarSesion = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const addAdmin = () => {
+
+        if ((name!=null && email!=null) && samePassword){
+            Axios.post("http://localhost:4000/registrar/user", {
+            name: name,
+            email: email,
+            password: password
+            }).then(() => {
+            alert("Registro exitoso");
+            });
+        }else{
+            alert("Ocurrio un error, verifique que todos los campos sean ingresados y correctos");
+        }
+    }
+
+    const samePassword = () => {
+        if ((password!=null && confirmPassword!=null) && password==confirmPassword){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     return (
         <div className="registrar-sesion">
             <div className="primera-parteRS">
@@ -16,11 +50,35 @@ const RegistrarSesion = () => {
                     <h1>HERBARIO</h1>
                 </div>
                 <h1 className="subRS">Registrarse</h1>
-                <input type="name" placeholder="Nombre completo" className="inRS"/>
-                <input type="email" placeholder="Correo" className="inRS"/>
-                <input type="password" placeholder="Contraseña" className="inRS"/>
-                <input type="password" placeholder="Confirmar contraseña" className="inRS"/>
-                <button className="botonRS">Registrarse</button>
+                <input
+                    type="name"
+                    placeholder="Nombre completo"
+                    className="inRS"
+                    onChange={(event)=>{
+                        setName(event.target.value);
+                    }}/>
+                <input
+                    type="email"
+                    placeholder="Correo"
+                    className="inRS"
+                    onChange={(event)=>{
+                        setEmail(event.target.value);
+                    }}/>
+                <input
+                    type="password"
+                    placeholder="Contraseña"
+                    className="inRS"
+                    onChange={(event)=>{
+                        setPassword(event.target.value);
+                    }}/>
+                <input
+                    type="password"
+                    placeholder="Confirmar contraseña"
+                    className="inRS"
+                    onChange={(event)=>{
+                        setConfirmPassword(event.target.value);
+                    }}/>
+                <button className="botonRS" onClick={addAdmin}>Registrarse</button>
                 <h1 className="textRS">¿Ya tienes cuenta? <span><Link to='/iniciarsesion' className="linkRSD"> Iniciar sesión</Link></span></h1>
             </div>
         </div>
