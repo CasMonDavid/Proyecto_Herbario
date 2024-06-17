59fbdbc93c8a613c1f5ea2de9e9cd0f63cc4896a
CasMonDavid <dcastro_21@alu.uabcs.mx>
2024-06-17 03:31:12 -0700

    /editar plantas para admin finalizado

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -33,11 +33,13 @@ app.get("/plantas/getall", async (req, res, next) => {
 app.get("/editar/:id", async (req, res, next) => {
     try{
         const id = req.params.id;
-        const [result] = await connection.execute(`SELECT * FROM plantas WHERE id_planta = ?`,[id]);
+        const [result] = await connection.execute("SELECT * FROM plantas WHERE id_planta = ?",[id]);
         if (result.length === 0) {
             return res.status(404).send("Planta no encontrada");
         }
-        res.json(result);
+        const planta = result[0];
+        planta.fecha_recoleccion = new Date(planta.fecha_recoleccion).toISOString().split('T')[0];
+        res.json(planta);
     }catch(err){
         console.log(err);
         res.status(500).send("Error al obtener la planta");
@@ -51,6 +53,38 @@ app.get("/plantasadmin/getall", async (req, res, next) => {
     } catch (err) {
         console.log(err);
         res.status(500).send("Error al obtener la lista de plantas");
+    }
+});
+
+app.put('/editar/:id', async (req, res) => {
+    const id = req.params.id;
+    const { numero_catalogo, id_ocurrencia, nombre_cientifico, nombre_comun, taxon, id_familia, id_colector, fecha, id_localidad, id_habitat, fotografia, id_investigador } = req.body;
+
+    if (
+        numero_catalogo === undefined || id_ocurrencia === undefined ||
+        nombre_cientifico === undefined || nombre_comun === undefined ||
+        taxon === undefined || id_familia === undefined ||
+        id_colector === undefined || fecha === undefined ||
+        id_localidad === undefined || id_habitat === undefined ||
+        fotografia === undefined || id_investigador === undefined
+    ) {
+        return res.status(400).json({ message: 'Faltan campos requeridos en el cuerpo de la solicitud' });
+    }
+    
+    try{
+        const formattedDate = new Date(fecha).toISOString().split('T')[0];
+        const [result] = await connection.execute(
+            'UPDATE plantas SET numero_catalago = ?, id_ocurrencia = ?, nombre_cientifico = ?, nombre_comun = ?, taxon = ?, id_familia = ?, id_colector = ?, fecha_recoleccion = ?, id_localidad = ?, id_habitat = ?, fotografia = ?, id_investigador = ? WHERE id_planta = ?',
+        [numero_catalogo, id_ocurrencia, nombre_cientifico, nombre_comun, taxon, id_familia, id_colector, fecha, id_localidad, id_habitat, fotografia, id_investigador, id]);
+
+        if (result.affectedRows > 0) {
+            res.json({ message: 'Planta actualizada correctamente' });
+        } else {
+            res.status(404).json({ message: 'Planta no encontrada' });
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).send("Error al actualizar la planta");
     }
 });
 
--- frontend/src/components/paginas/editarPlantaAdmin/editar.jsx
+++ frontend/src/components/paginas/editarPlantaAdmin/editar.jsx
@@ -9,57 +9,211 @@ const Editar = () => {
     console.log("Id: "+id);
 
     const [planta, setPlanta] = useState([]);
-    const [url, setUrl] = useState("");
-
+    const [numeroCatalogo, setNumeroCatalogo] = useState(0);
+    const [idOcurrencia, setIdOcurrencia] = useState("");
+    const [nombreCientifico, setNombreCientifico] = useState("");
+    const [fecha, setFecha] = useState("");
+    const [nombreComun, setNombreComun] = useState("");
+    const [taxon, setTaxon] = useState("");
+    const [idFamilia, setIdFamilia] = useState(0);
+    const [idColector, setIdColector] = useState(0);
+    const [idLocalidad, setIdLocalidad] = useState(0);
+    const [idHabitat, setIdHabitat] = useState(0);
+    const [fotografia, setFotografia] = useState("");
+    const [idInvestigador, setIdInvestigador] = useState(0);
+    
     useEffect(() => {
         Axios.get(`http://localhost:4000/editar/${id}`)
             .then(response => {
                 setPlanta(response.data);
-                setUrl(planta.fotografia);
+                setFotografia(response.data.fotografia);
+                setFecha(response.data.fecha_recoleccion);
+                setNombreCientifico(response.data.nombre_cientifico);
+                setNombreComun(response.data.nombre_comun);
+                setNumeroCatalogo(response.data.numero_catalago);
+                setIdOcurrencia(response.data.id_ocurrencia);
+                setTaxon(response.data.taxon);
+                setIdFamilia(response.data.id_familia);
+                setIdColector(response.data.id_colector);
+                setFecha(response.data.fecha_recoleccion);
+                setIdLocalidad(response.data.id_localidad);
+                setIdHabitat(response.data.id_habitat);
+                setIdInvestigador(response.data.id_investigador);
+
+                console.log(response.data.taxon);
             })
             .catch(error => {
                 console.error("Hubo un error al obtener los datos:", error);
             });
-    }, []); 
+    }, [id]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const formattedDate = new Date(fecha).toISOString().split('T')[0];
+
+        Axios.put(`http://localhost:4000/editar/${id}`, {
+            numero_catalogo: numeroCatalogo,
+            id_ocurrencia: idOcurrencia,
+            nombre_cientifico: nombreCientifico,
+            nombre_comun: nombreComun,
+            taxon: taxon,
+            id_familia: idFamilia,
+            id_colector: idColector,
+            fecha: formattedDate,
+            id_localidad: idLocalidad,
+            id_habitat: idHabitat,
+            fotografia: fotografia,
+            id_investigador: idInvestigador
+        }).then(response => {
+            alert("Planta actualizada de forma correcta");
+        }).catch(error => {
+            console.error("Hubo un error al actualizar los datos:", error);
+            alert("Error al actualizar los datos");
+        });
+    };
+
+    const numeroCatalogoChange = (event) => {
+        setNumeroCatalogo(event.target.value);
+    };
+    const idOcurrenciaChange = (event) => {
+        setIdOcurrencia(event.target.value);
+    };
+    const nombreCientificoChange = (event) => {
+        setNombreCientifico(event.target.value);
+    };
+    const nombreComunChange = (event) => {
+        setNombreComun(event.target.value);
+    };
+    const taxonChange = (event) => {
+        setTaxon(event.target.value);
+    };
+    const idFamiliaChange = (event) => {
+        setIdFamilia(event.target.value);
+    };
+    const idColectorChange = (event) => {
+        setIdColector(event.target.value);
+    };
+    const fechaChange = (event) => {
+        setFecha(event.target.value);
+    };
+    const idLocalidadChange = (event) => {
+        setIdLocalidad(event.target.value);
+    };
+    const idHabitatChange = (event) => {
+        setIdHabitat(event.target.value);
+    };
+    const fotografiaChange = (event) => {
+        setFotografia(event.target.value);
+    };
+    const idInvestigadorChange = (event) => {
+        setIdInvestigador(event.target.value);
+    };
 
     return (
         <div className="editar-bg">
             <div className="editar-uno">
-                <img src="https://images.pexels.com/photos/15845935/pexels-photo-15845935/free-photo-of-madera-naturaleza-caminando-bosque.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="img"  className="imgn-edr"/> {/* AQUI VA LA IMAGEN COLOCADA EN LA BASE DE DATOS */}
+                <img src={fotografia} alt="Fotogracia de la planta"  className="imgn-edr"/> {/* AQUI VA LA IMAGEN COLOCADA EN LA BASE DE DATOS */}
                 <input
                     type="url"
                     className="in-ep"
                     placeholder="URL de la imagen"
-                    defaultValue= {url}
+                    value={fotografia}
+                    onChange={fotografiaChange}
                 />
                 <div className="nam-sub-ed"><h1 className="sub-ed">Nombre cientifico</h1></div>
-                <input type="text" className="in-ep" placeholder="Nombre cientifico"/>
+                <input
+                    type="text"
+                    className="in-ep"
+                    placeholder="Nombre cientifico"
+                    value={nombreCientifico}
+                    onChange={nombreCientificoChange}
+                />
                 <div className="nam-sub-ed"><h1 className="sub-ed">Nombre común</h1></div>
-                <input type="text" className="in-ep" placeholder="Nombre común"/>
+                <input
+                    type="text"
+                    className="in-ep"
+                    placeholder="Nombre común"
+                    value={nombreComun ? nombreComun : ''}
+                    onChange={nombreComunChange}
+                />
                 <div className="nam-sub-ed"><h1 className="sub-ed">Número de catalogo</h1></div>
-                <input type="text" className="in-ep" placeholder="Número de catalogo"/>
+                <input
+                    type="text"
+                    className="in-ep"
+                    placeholder="Número de catalogo"
+                    value={numeroCatalogo}
+                    onChange={numeroCatalogoChange}
+                />
             </div>
             <div className="editar-dos">
                 <div className="nam-sub-ed"><h1 className="sub-ed">Id: {id}</h1></div>
                 <div className="nam-sub-ed"><h1 className="sub-ed">Id concurrencia</h1></div>
-                <input type="text" className="in-ep" placeholder="Id concurrencia"/>
+                <input
+                    type="text"
+                    className="in-ep"
+                    placeholder="Id concurrencia"
+                    value={idOcurrencia}
+                    onChange={idOcurrenciaChange}
+                />
                 <div className="nam-sub-ed"><h1 className="sub-ed">Taxon</h1></div>
-                <input type="text" className="in-ep" placeholder="Taxon"/>
+                <input
+                    type="text"
+                    className="in-ep"
+                    placeholder="Taxon"
+                    value={taxon}
+                    onChange={taxonChange}
+                />
                 <div className="nam-sub-ed"><h1 className="sub-ed">Id familia</h1></div>
-                <input type="text" className="in-ep" placeholder="Id familia"/>
+                <input
+                    type="text"
+                    className="in-ep"
+                    placeholder="Id familia"
+                    value={idFamilia}
+                    onChange={idFamiliaChange}
+                />
                 <div className="nam-sub-ed"><h1 className="sub-ed">Id colector</h1></div>
-                <input type="text" className="in-ep" placeholder="Id colector"/>
+                <input
+                    type="text"
+                    className="in-ep"
+                    placeholder="Id colector"
+                    value={idColector}
+                    onChange={idColectorChange}
+                />
                 <div className="nam-sub-ed"><h1 className="sub-ed">Fecha recoleccion</h1></div>
-                <input type="date" className="in-ep"/>
+                <input
+                    type="text"
+                    className="in-ep"
+                    placeholder="AAAA-MM-DD"
+                    value={fecha}
+                    onChange={fechaChange}
+                />
                 <div className="nam-sub-ed"><h1 className="sub-ed">Id localidad</h1></div>
-                <input type="text" className="in-ep" placeholder="Id localidad"/>
+                <input
+                    type="text"
+                    className="in-ep"
+                    placeholder="Id localidad"
+                    value={idLocalidad}
+                    onChange={idLocalidadChange}
+                />
                 <div className="nam-sub-ed"><h1 className="sub-ed">Id habitad</h1></div>
-                <input type="text" className="in-ep" placeholder="Id habitad"/>
+                <input
+                    type="text"
+                    className="in-ep"
+                    placeholder="Id habitad"
+                    value={idHabitat}
+                    onChange={idHabitatChange}
+                />
                 <div className="nam-sub-ed"><h1 className="sub-ed">Id investigador</h1></div>
-                <input type="text" className="in-ep" placeholder="Id investigador"/>
-
+                <input
+                    type="number"
+                    className="in-ep"
+                    placeholder="Id investigador"
+                    value={idInvestigador}
+                    onChange={idInvestigadorChange}
+                />
                 <div>
-                    <button className="boton-ep ">Guardar</button>
+                    <button onClick={handleSubmit} className="boton-ep ">Guardar</button>
                 </div>
 
             </div>
