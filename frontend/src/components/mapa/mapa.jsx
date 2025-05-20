e2401ab1e82178289f81c970bff61009419a62f9
CasMonDavid <dcastro_21@alu.uabcs.mx>
2025-05-19 23:41:39 -0700

    Frontend: implementación de un mapa

--- frontend/src/components/mapa/mapa.jsx
+++ frontend/src/components/mapa/mapa.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import './mapa.css'
-import mapa from './mapa.PNG';
+
+//
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const Mapa = () => {
     return (
-        <div className="mapa">
-            <h1>Mapa</h1>
-            <div className="mapaD">
-                <img className="mapa-img" src={mapa} alt="mapa" />
-            </div>
-        </div>
+        <MapContainer center={[20.6597, -103.3496]} zoom={6} style={{ height: "400px", width: "100%" }}>
+            <TileLayer
+                attribution='&copy; OpenStreetMap contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[24.102626, -110.316013]}>
+                <Popup>
+                    Aquí se encontró una planta.
+                </Popup>
+            </Marker>
+        </MapContainer>
     );
 }
 
