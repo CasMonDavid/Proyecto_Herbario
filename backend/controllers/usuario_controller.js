0f16789fb53440a172afbbbc0277465e33d33cf8
CasMonDavid <dcastro_21@alu.uabcs.mx>
2025-06-02 23:20:25 -0700

    Back: hashear contraseñas

--- backend/controllers/usuario_controller.js
+++ backend/controllers/usuario_controller.js
@@ -1,14 +1,25 @@
 const connection = require('../config/db');
+const bcrypt = require('bcrypt');
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const [result] = await connection.query('SELECT * FROM investigadores WHERE correo_electronico = ? AND contrasena = ?', [email, password]);
-    if (result.length > 0) {
-      res.json({ success: true, data: result[0] });
-    } else {
-      res.json({ success: false });
+    const [result] = await connection.query('SELECT * FROM investigadores WHERE correo_electronico = ?',
+        [email]);
+    if (result.length === 0) {
+      res.status(404).json({ success: false, message: "Usuario no encontrado" });
     }
+
+    const usuario = result[0];
+
+    const match = await bcrypt.compare(password, usuario.contrasena);
+
+    if (match) {
+      res.json({ success: true, data: usuario });
+    } else {
+      res.json({ success: false, message: "Contraseña incorrecta" });
+    }
+
   } catch (err) {
     console.log(err);
     res.status(500).send("Error al iniciar sesión");
@@ -19,9 +30,11 @@ exports.createUser = async (req,res, next)=>{
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
-        const [result] = await connection.query('INSERT INTO investigadores(nombre, correo_electronico, contrasena, codigo_acceso) VALUES(?,?,?,?)',[name,email,password,123456789]);
+        const [result] = await connection.query('INSERT INTO investigadores(nombre, correo_electronico, contrasena, codigo_acceso) VALUES(?,?,?,?)',
+            [name,email,hashedPassword,123456789]);
         res.send("Investigador registrado con éxito!!");
     }catch(err) {
         console.log(err);
@@ -32,15 +45,16 @@ exports.createUser = async (req,res, next)=>{
 exports.editUserById = async (req, res) => {
     const id = req.params.id;
     const { nombre, correo_electronico, contrasena } = req.body;
-
+    
     if (nombre === undefined || correo_electronico === undefined || contrasena === undefined) {
         return res.status(400).json({ message: 'Faltan campos requeridos en el cuerpo de la solicitud' });
     }
+    const hashedPassword = await bcrypt.hash(contrasena, 10);
     
     try{
         const [result] = await connection.execute(
             'UPDATE investigadores SET nombre = ?, correo_electronico = ?, contrasena = ?, codigo_acceso = ? WHERE id_investigador = ?',
-        [nombre, correo_electronico, contrasena, 123456789, id]);
+        [nombre, correo_electronico, hashedPassword, 123456789, id]);
 
         if (result.affectedRows > 0) {
             res.json({ message: 'Usuario actualizado correctamente' });
--- backend/routes/usuario_routes.js
+++ backend/routes/usuario_routes.js
@@ -2,11 +2,30 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuario_controller');
 
+const connection = require('../config/db');
+const bcrypt = require('bcrypt');
+
 router.post("/iniciarsesion/investigador", usuarioController.login);
 router.put('/usuarioedit/:id', usuarioController.editUserById);
 router.post("/iniciarsesion/investigador", usuarioController.login);
 router.post("/registrar/user", usuarioController.createUser);
 router.get("/usuarioedit/:id", usuarioController.getUserById);
 router.get("/administrarusuarios/getall", usuarioController.getAllUsers);
+
+router.get("/hashearpasswords", async (req, res) => {
+    try {
+        const [usuarios] = await connection.query("SELECT id_investigador, contrasena FROM investigadores");
+
+        for (const usuario of usuarios) {
+            const hashedPassword = await bcrypt.hash(usuario.contrasena, 10);
+            await connection.query("UPDATE investigadores SET contrasena = ? WHERE id_investigador = ?",
+                [hashedPassword, usuario.id_investigador]);
+        }
+        res.json({ success: true, message: "Todas las contraseñas han sido hasheadas correctamente" })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Ocurrio un error al querer hashear las contraseñas", error: error.message })
+    }
+});
 
 module.exports = router;
