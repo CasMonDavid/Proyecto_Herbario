1bdcc8e22a6cb0a1f4dc6f14526e6d12cbea84d7
CarlosSanchez22 <90216758+CarlosSanchez22@users.noreply.github.com>
2025-09-01 19:23:34 -0700

    Se agrando campo para ver detalle planta

--- frontend/src/components/paginas/plantas/card.jsx
+++ frontend/src/components/paginas/plantas/card.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import './card.css';
 import { Link } from "react-router-dom";
-import ver from "./ojo.png";
 
-const Card = ({ title, id, imageUrl}) => {
+const Card = ({ title, id, imageUrl }) => {
     const baseUrl = "http://localhost:4000";
     return (
         <div className="card" style={{ width: "18rem", marginTop: "10px" }}>
             <div className="card-body">
-            <img src={`${baseUrl}/${imageUrl}`} className="card-img-top" alt={title} />
-            <div className="atriA">
-                <Link to={`/informacion/${id}`} className="card-img-butt-2">
-                    <img src={ver} className="card-img-denA" alt="ver" />
+                {/* Aquí envolvemos la imagen completa en el Link */}
+                <Link to={`/informacion/${id}`}>
+                    <img 
+                        src={`${baseUrl}/${imageUrl}`} 
+                        className="card-img-top" 
+                        alt={title} 
+                        style={{ cursor: "pointer" }} 
+                    />
                 </Link>
-            </div>
-            <h5 className="card-title">{title}</h5>
+                
+                {/* El nombre de la planta también lo puedes hacer clickeable si quieres */}
+                <h5 className="card-title">{title}</h5>
             </div>
         </div>
     );
-}
+};
 
 export default Card;
