9aedc044f890fac24ca94c4b0b29aa89f24d5f83
CarlosSanchez22 <90216758+CarlosSanchez22@users.noreply.github.com>
2025-10-05 17:45:13 -0700

    Se implemento buscador

--- frontend/src/components/paginas/resultadoBuscador/resultadoBuscador.jsx
+++ frontend/src/components/paginas/resultadoBuscador/resultadoBuscador.jsx
@@ -1,14 +1,62 @@
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom"; // <- Asegúrate de importar useNavigate
+import Axios from "axios";
+import Card from "../plantas/card";
+import "../plantas/plantas.css";
+import "./resultadoBuscador.css"; 
 
 function ResultadoBuscador() {
+  const { query } = useParams();
+  const navigate = useNavigate(); // <- Aquí declaras navigate
+  const [resultados, setResultados] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const { query } = useParams();
+  useEffect(() => {
+    if (!query) return;
 
-    return(
-        <div>
-            <h1>Resultados para: {decodeURIComponent(query)}</h1>
-        </div>
-    )
+    setIsLoading(true);
+    Axios.post("http://localhost:4000/plantas/search", { q: query })
+      .then((response) => {
+        setResultados(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error al buscar:", error);
+        setIsLoading(false);
+      });
+  }, [query]);
+
+  return (
+    <div className="resultado-container">
+  <button className="btn-volver" onClick={() => navigate(-1)}>
+    ⬅ Volver
+  </button>
+
+  <h1 className="resultado-titulo">
+    Resultados para: <span>{decodeURIComponent(query)}</span>
+  </h1>
+
+  {isLoading ? (
+    <p>Cargando resultados...</p>
+  ) : resultados.length > 0 ? (
+    <div className="cartas">
+      {resultados.map((planta) => (
+        <Card
+          key={planta.id_planta}
+          id={planta.id_planta}
+          title={planta.nombre_cientifico}
+          imageUrl={planta.fotografia}
+        />
+      ))}
+    </div>
+  ) : (
+    <p>No se encontraron resultados.</p>
+  )}
+</div>
+
+
+
+  );
 }
 
 export default ResultadoBuscador;
