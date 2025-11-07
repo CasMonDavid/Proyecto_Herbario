7ef56389b8ada4059f91d54cb15aa9e13b533954
CarlosSanchez22 <90216758+CarlosSanchez22@users.noreply.github.com>
2025-11-06 21:58:13 -0700

    mejora de diseño mapa descubrimientos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 //import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 //import Axios from "axios";
 import Menu from "./components/menu/menu";
 import Footer from "./components/footer/footer";
@@ -20,42 +20,50 @@ import RegistrarDescubrimiento from './components/paginas/registrarDescubrimient
 import EditarDescubrimiento from './components/paginas/editarDescubrimiento/editarDescubrimiento';
 import ResultadoBusqueda from './components/paginas/resultadoBuscador/resultadoBuscador';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+  const isMapaPage = location.pathname === '/descubrimientos';
+
   return (
-    <Router>
-      <div className="App">
-        <Menu />
+    <div className="App">
+      <Menu />
 
-        {/* Contenedor principal */}
-        <div className="content">
-          {/* Inner wrapper para padding-bottom y elementos absolutos */}
-          <div className="content-inner">
-            <Routes>
-              <Route path='/' element={<Inicio />} />
-              <Route path='/iniciarsesion' element={<IniciarSesion />} />
-              <Route path='/plantas' element={<Plantas />} />
-              <Route path='/registrar' element={<RegistrarSesion />} />
-              <Route path='/plantasadmin' element={<PlantasAdmin />} />
-              <Route path='/editar/:id' element={<Editar />} />
-              <Route path='/informacion/:id' element={<Informacion />} />
-              <Route path='/usuario' element={<Usuario />} />
-              <Route path='/usuarioedit/:id' element={<UsuarioEditar />} />
-              <Route path='/administrarusuarios' element={<AdministrarUsuarios />} />
-              <Route path='/registrarplanta' element={<RegistrarPlanta />} />
-              <Route path='/descubrimientos' element={<DescubrimientosMapa />} />
-              <Route path='/descubrimientos/agregar' element={<RegistrarDescubrimiento />} />
-              <Route path='/descubrimiento/:id' element={<EditarDescubrimiento />} />
-              <Route path='/resultado/:query' element={<ResultadoBusqueda />} />
-            </Routes>
-          </div>
+      {/* Contenedor principal */}
+      <div className="content">
+        {/* Inner wrapper para padding-bottom y elementos absolutos */}
+        <div className={`content-inner ${isMapaPage ? 'no-footer-space' : ''}`}>
+          <Routes>
+            <Route path='/' element={<Inicio />} />
+            <Route path='/iniciarsesion' element={<IniciarSesion />} />
+            <Route path='/plantas' element={<Plantas />} />
+            <Route path='/registrar' element={<RegistrarSesion />} />
+            <Route path='/plantasadmin' element={<PlantasAdmin />} />
+            <Route path='/editar/:id' element={<Editar />} />
+            <Route path='/informacion/:id' element={<Informacion />} />
+            <Route path='/usuario' element={<Usuario />} />
+            <Route path='/usuarioedit/:id' element={<UsuarioEditar />} />
+            <Route path='/administrarusuarios' element={<AdministrarUsuarios />} />
+            <Route path='/registrarplanta' element={<RegistrarPlanta />} />
+            <Route path='/descubrimientos' element={<DescubrimientosMapa />} />
+            <Route path='/descubrimientos/agregar' element={<RegistrarDescubrimiento />} />
+            <Route path='/descubrimiento/:id' element={<EditarDescubrimiento />} />
+            <Route path='/resultado/:query' element={<ResultadoBusqueda />} />
+          </Routes>
         </div>
-
-        <Footer />
       </div>
-    </Router>
+
+      {/*  Solo muestra el footer si NO está en la página de mapa */}
+      {!isMapaPage && <Footer />}
+    </div>
   );
 }
 
-
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
 
 export default App;
